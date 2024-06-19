46b8041d7368f397b3c82e4af965a0a002333e93
CAUAN ORTIZ <0031432412004@lti.fatecso.edu.br>
2024-06-19 15:59:38 -0300

    1906

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,18 +37,19 @@
                     <p>Sign in with OTP</p>
                     <p>Forgot Password?</p>
                 </div>
-
-                <input type="submit" value="Login" class="form-btn">
-
-                <div class="form-connections">
-                    <p>Or connect with</p>
-                    <div>
-                        <i>Google</i>
-                        <i>Facebook</i>
-                        <i>Apple Connect</i>
-                    </div>
-                    <p>Don't have an account?</p>
+                <div class="form-low">
+                    <input type="submit" value="Login" class="form-btn">
+                    <div class="form-connections">
+                        <p>Or connect with</p>
+                        <div class="connections">
+                            <img src="https://img.freepik.com/free-icon/google_318-278809.jpg" alt="">
+                            <img src="https://cdn.freebiesupply.com/logos/large/2x/facebook-3-logo-png-transparent.png" alt="">
+                            <img src="https://th.bing.com/th/id/R.1ec11a869384bc5e59625bac39b6a099?rik=1dlGqAp84GWGFw&riu=http%3a%2f%2fpngimg.com%2fuploads%2fapple_logo%2fapple_logo_PNG19692.png&ehk=5ghp5P0aLzQqfUKTsPihTYaIP%2b4VcHGKNovcBq8KOuo%3d&risl=&pid=ImgRaw&r=0" alt="">
+                        </div>
+                        <p>Don't have an account? <strong>Sign Up</strong></p>
+                    </div>          
                 </div>
+                
             </form>
         </div>
         <div class="form-right">
